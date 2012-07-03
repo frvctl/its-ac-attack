f9998e71428b36f86fa4b229728e3712181cd5fe
Epiccodez <epiccodez@gmail.com>
2012-07-03 00:02:27 -0400

    Fixed the facebook authorization by adding keys for this application

--- app/controllers/users-controller.js
+++ app/controllers/users-controller.js
@@ -1,6 +1,20 @@
-var User = mongoose.model('User')
+var User = mongoose.model('User');
 
 module.exports = function (app) {
+
+  // Handles Sign-up
+  app.get('/signup', function(req, res){
+    res.render('users/signup', {
+      title: 'Sign Up'
+    });
+  });
+
+  // Handles Session Login
+  app.get('/login', function(req, res){
+    res.render('users/login', {
+      title: 'Login'
+    });
+  });
 
   app.param('profileId', function (req, res, next, id) {
     User
@@ -10,23 +24,38 @@ module.exports = function (app) {
         if (!user) return next(new Error('Failed to load User ' + id))
         req.foundUser = user
         next()
-      })
-  })
+      });
+  });
 
   // Handles session Logout
   app.get('/logout', function (req, res) {
-    req.logout()
+    req.logout(),
     res.redirect('/home')
-  })
+  });
 
   // Profile view
   app.get('/profile/:profileId', function (req, res) {
-    var user = req.foundUser
+    var user = req.foundUser;
     res.render('users/profile', {
-        title : user.fb.name.full
-      , user : user
-    })
-  })
+        title : user.fb.name.full,
+        user : user
+    });
+  });
 
+  // Access controll middleware
+  function loadUser(req, res, next){
+    if(req.session.user_id){
+      User.findById(req.session.user_id, function(user){
+        if(user){
+          req.currentUser = user;
+          next();
+        }else{
+          res.redirect('/sessions/new');
+        }
+      });
+    }else{
+      res.redirect('/sessions/new');
+    }
+  }
 
-}
+};
--- app/models/user.js
+++ app/models/user.js
@@ -4,104 +4,111 @@ var exports = module.exports = everyauth = require('everyauth')
   , User
 
 
-var exports = module.exports = mongooseAuth = require('mongoose-auth')
+var exports = module.exports = mongooseAuth = require('mongoose-auth');
 
-everyauth.debug = true
+everyauth.debug = true;
 
 // This is how you request permissions
-everyauth.facebook.scope('email, user_about_me, user_location')
+everyauth.facebook.scope('email, user_about_me, user_location');
 
 // Eleminate timeout completely
-everyauth.facebook.moduleTimeout(-1)
+everyauth.facebook.moduleTimeout(-1);
 
 // To see all the configurable options
 // console.log(everyauth.facebook.configurable())
 
 UserSchema.add({
-    bio: String
-  , location: {
+    bio: String,
+    location: {
       name: String
-    }
-  , fbProfileUrl: String
-  , created_at  : {type : Date, default : Date.now}
-})
+    },
+    fbProfileUrl: String,
+    created_at  : {type : Date, default : Date.now}
+});
 
 
 UserSchema.plugin(mongooseAuth, {
     everymodule: {
       everyauth: {
         User: function () {
-          return User
+          return User;
         }
       }
-    }
-  , facebook: {
+    },
+    facebook: {
       everyauth: {
-          myHostname: config.facebook.host_uri
-        , appId: config.facebook.appId
-        , appSecret: config.facebook.appSecret
-        , redirectPath: '/'
-        , findOrCreateUser: function (sess, accessTok, accessTokExtra, fbUser) {
-            var promise = this.Promise()
-              , User = this.User()()
+          myHostname: config.facebook.host_uri,
+          appId: config.facebook.appId,
+          appSecret: config.facebook.appSecret,
+          redirectPath: '/',
+          findOrCreateUser: function (sess, accessTok, accessTokExtra, fbUser) {
+            var promise = this.Promise(),
+                User = this.User()();
             // TODO Check user in session or request helper first
             //      e.g., req.user or sess.auth.userId
             User.findOne({'fb.id': fbUser.id}, function (err, foundUser) {
-              if (err) return promise.fail(err)
+              if (err) return promise.fail(err);
               if (foundUser) {
-                return promise.fulfill(foundUser)
+                return promise.fulfill(foundUser);
               }
-              console.log("CREATING")
+              console.log("CREATING");
 
-              var expiresDate = new Date
-              expiresDate.setSeconds(expiresDate.getSeconds() + accessTokExtra)
+              var expiresDate = new Date;
+              expiresDate.setSeconds(expiresDate.getSeconds() + accessTokExtra);
 
               user = new User({
                   fb: {
-                      id: fbUser.id
-                    , accessToken: accessTok
-                    , expires: expiresDate
-                    , name: {
-                          full: fbUser.name
-                        , first: fbUser.first_name
-                        , last: fbUser.last_name
-                      }
-                    , alias: fbUser.link.match(/^http:\/\/www.facebook\.com\/(.+)/)[1]
-                    , gender: fbUser.gender
-                    , email: fbUser.email
-                    , timezone: fbUser.timezone
-                    , locale: fbUser.locale
-                    , updatedTime: fbUser.updated_time
-                  }
-                , fbProfileUrl: fbUser.link
-                , bio: fbUser.bio
-                , location: {
+                      id: fbUser.id,
+                      accessToken: accessTok,
+                      expires: expiresDate,
+                      name: {
+                          full: fbUser.name,
+                          first: fbUser.first_name,
+                          last: fbUser.last_name
+                      },
+                      alias: fbUser.link.match(/^http:\/\/www.facebook\.com\/(.+)/)[1],
+                      gender: fbUser.gender,
+                      email: fbUser.email,
+                      timezone: fbUser.timezone,
+                      locale: fbUser.locale,
+                      updatedTime: fbUser.updated_time
+                  },
+                  fbProfileUrl: fbUser.link,
+                  bio: fbUser.bio,
+                  location: {
                     name: fbUser.location && fbUser.location.name ? fbUser.location.name : ''
                   }
-              })
+              });
 
               user.save( function (err, savedUser) {
-                promise.fulfill(savedUser)
-              })
-            })
+                promise.fulfill(savedUser);
+              });
+            });
 
-            return promise
+            return promise;
           }
       }
     }
-})
+});
 
 // validations
+UserSchema.path('twitter.name.full').validate(function(name){
+  return name.trim().split(' ').length >= 2;
+}, 'Please provide your full name');
+
+UserSchema.path('twitter.email').validate(function (email) {
+  return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i.test(email);
+}, 'Please provide a proper email');
 
 UserSchema.path('fb.name.full').validate(function (name) {
-  return name.trim().split(' ').length >= 2
-}, 'Please provide your fullname')
+  return name.trim().split(' ').length >= 2;
+}, 'Please provide your fullname');
 
 UserSchema.path('fb.email').validate(function (email) {
-  return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i.test(email)
-}, 'Please provide a proper email')
+  return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i.test(email);
+}, 'Please provide a proper email');
 
 
 // virtual attributes
 
-var exports = module.exports = User = mongoose.model('User', UserSchema)
+var exports = module.exports = User = mongoose.model('User', UserSchema);
