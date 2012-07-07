2dc6eddc990c19f348357b4455b6dcd192c95cfb
EpicCodez <Epiccodez@gmail.com>
2012-07-06 23:30:42 -0400

    Made the search function asynchronous and faster

--- app/controllers/practice-controller.js
+++ app/controllers/practice-controller.js
@@ -1,4 +1,9 @@
+var Question = mongoose.model('Question')
+  , questions = [];
+
 module.exports = function(app){
+    
+
 	app.get('/practice', function(req, res){
 		res.render('questions/practice', {
 			title: 'Practice'
--- app/controllers/search-controller.js
+++ app/controllers/search-controller.js
@@ -1,29 +1,24 @@
-var Question = mongoose.model('Question')
-  , questions = []
-  , searchIndx = '';
+var Question = mongoose.model('Question');
 
 module.exports = function(app){
-  function getJson(searchIndx){
-      Question.find({$or :                    // $or is similar to logic OR also RegEx allows for partial searchs
-          [{category: {$regex: searchIndx}}   // Searches by Categories
-          , {answer:{$regex: searchIndx}}     // Searches by Answers
-          , {difficulty:{$regex: searchIndx}} // Searches by Difficulty
-            ]}, function(err, items){         // Searches by Year
-        questions = items;
-        console.log("Error "+ err);
+  app.get('/search', function(req, res){
+    searchIndx = req.query.searchInput;
+    if(searchIndx){
+    Question.find({$or :                  // $or is similar to logical || also RegEx allows for partial searchs
+      [ {category: {$regex: searchIndx}}  // Searches by Categories
+      , {answer:{$regex: searchIndx}}     // Searches by Answers
+      , {difficulty:{$regex: searchIndx}} // Searches by Difficulty
+      ] }, function(err, questions){      // Searches by Year
+    res.render('questions/search', {
+      title: 'Search',
+      questions: questions
       });
-  }
-
-    app.get('/search', function(req, res){
-      searchIndx = req.query.searchInput;
-      if(req.query.searchInput){
-        getJson(searchIndx);
-      }else{
-        searchIndx = '';
-      }
+    });
+    }else{
       res.render('questions/search', {
         title: 'Search',
-        questions: questions
+        questions: []
       });
-   });
- };
+    }
+  });
+};
