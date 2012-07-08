2c707f4753827d3d58e3535a4de0c0c3de70ce7e
EpicCodez <Epiccodez@gmail.com>
2012-07-08 16:02:46 -0400

    Started work on the practice component

--- app/controllers/practice-controller.js
+++ app/controllers/practice-controller.js
@@ -1,23 +1,27 @@
-var Question = mongoose.model('Question');
+var Question = mongoose.model('Question'),
+    questionAns = [];
 
 module.exports = function(app){
   app.get('/practice', function(req, res){
     searchIndx = 'Dickens';
+    userAnswer = req.query.answerInput;
+    console.log(userAnswer);
     if(searchIndx){
-    Question.find({$or :                  // $or is similar to logical || also RegEx allows for partial searchs
-      [ {category: {$regex: searchIndx}}  // Searches by categories
-      , {answer:{$regex: searchIndx}}     // Searches by answers
-      , {difficulty:{$regex: searchIndx}} // Searches by difficulty
-      , {question: {$regex: searchIndx}}  // Searches the question
-      , {year: {$type: 18}}               // Searches by year
-      ] }, function(err, questions){      
-    res.render('questions/practice', {
-      title: 'Practice',
-      questions: questions,
-      maxNum: 10,
-      minNum: 0
+      Question.find({$or :                   // $or is similar to logical || also RegEx allows for partial searchs
+        [ {category: {$regex: searchIndx}},  // Searches by categories
+          {answer:{$regex: searchIndx}},     // Searches by answers
+          {difficulty:{$regex: searchIndx}}, // Searches by difficulty
+          {question: {$regex: searchIndx}},  // Searches the question
+          {year: {$type: 18}}                // Searches by year
+        ] }, function(err, questions){
+      console.log(questions);
+      res.render('questions/practice', {
+        title: 'Practice',
+        questions: questions,
+        maxNum: 10,
+        minNum: 0
+        });
       });
-    });
     }else{
       res.render('questions/practice', {
         title: 'Practice',
--- app/controllers/search-controller.js
+++ app/controllers/search-controller.js
@@ -1,29 +1,33 @@
-var Question = mongoose.model('Question');
+var Question = mongoose.model('Question'),
+    currentFirst = 1,
+    maxNum = 10;
 
 module.exports = function(app){
   app.get('/search', function(req, res){
     searchIndx = req.query.searchInput;
     if(searchIndx){
-    Question.find({$or :                  // $or is similar to logical || also RegEx allows for partial searchs
-      [ {category: {$regex: searchIndx}}  // Searches by categories
-      , {answer:{$regex: searchIndx}}     // Searches by answers
-      , {difficulty:{$regex: searchIndx}} // Searches by difficulty
-      , {question: {$regex: searchIndx}}  // Searches the question
-      , {year: {$type: 18}}               // Searches by year
-      ] }, function(err, questions){      
-    res.render('questions/search', {
-      title: 'Search',
-      questions: questions,
-      maxNum: 20,
-      minNum: 0
+      Question.find({$or :                   // $or is similar to logical || also RegEx allows for partial searchs
+        [ {category: {$regex: searchIndx}},  // Searches by categories
+          {answer:{$regex: searchIndx}},     // Searches by answers
+          {difficulty:{$regex: searchIndx}}, // Searches by difficulty
+          {question: {$regex: searchIndx}},  // Searches the question
+          {year: {$type: 18}}                // Searches by year
+        ] }, function(err, questions){
+      res.render('questions/search', {
+        title: 'Search',
+        questions: questions,
+        currentFirst: currentFirst,
+        maxNum: maxNum,
+        minNum: currentFirst - 1
+        });
       });
-    });
     }else{
       res.render('questions/search', {
         title: 'Search',
         questions: [],
-        maxNum: 20,
-        minNum: 0
+        currentFirst: currentFirst,
+        maxNum: maxNum,
+        minNum: currentFirst - 1
       });
     }
   });
