423ba81f2e17fc9531595db94a6f6ad568fbb1ef
EpicCodez <Epiccodez@gmail.com>
2012-07-09 12:31:01 -0400

    FiAdded a better method of pagination which does not currently work

--- app/controllers/chat-controller.js
+++ app/controllers/chat-controller.js
@@ -1,93 +1,94 @@
-var Chat = mongoose.model('Chat');
+// var Chat = mongoose.model('Chat');
 
 module.exports = function(app){
-  var io = require('socket.io').listen(app);
+//   var io = require('socket.io').listen(app);
 
-  io.sockets.on('connection', function(socket){
+//   io.sockets.on('connection', function(socket){
 
-    socket.on('login attempt', function(data){
-      session.login(io, socket, data);
-    });
+//     socket.on('login attempt', function(data){
+//       session.login(io, socket, data);
+//     });
 
-    socket.on('logout attempt', function(data){
-      session.logout(io, socket, data);
-    });
+//     socket.on('logout attempt', function(data){
+//       session.logout(io, socket, data);
+//     });
 
-    socket.on('message', function(data){
-      message.message(io, socket, data);
-    });
+//     socket.on('message', function(data){
+//       message.message(io, socket, data);
+//     });
 
-    socket.on('disconnect', function(data){
-      session.disconnect(io, socket, data);
-    });
-  });
+//     socket.on('disconnect', function(data){
+//       session.disconnect(io, socket, data);
+//     });
+//   });
 
-  app.get('/chat', function(req, res, io, socket, data){
-    User.findOne({socket_id: socket.id
-    }, function(err, doc){
-      if(err){
-        socket.emit('error', {
-          message: 'error reading clients list'
-        });
-        return;
-      }
+//   app.get('/chat', function(req, res, io, socket, data){
+//     User.findOne({socket_id: socket.id
+//     }, function(err, doc){
+//       if(err){
+//         socket.emit('error', {
+//           message: 'error reading clients list'
+//         });
+//         return;
+//       }
 
-      if(!doc){
-        socket.emit('message error', {
-          message: 'client not found'
-        });
-        return;
-      }
+//       if(!doc){
+//         socket.emit('message error', {
+//           message: 'client not found'
+//         });
+//         return;
+//       }
 
-      var message = {
-        nickname: doc.nickname,
-        message: data.message
-      };
+//       var message = {
+//         nickname: doc.nickname,
+//         message: data.message
+//       };
 
-      io.sockets.emit('message', message);
-    });
+//       io.sockets.emit('message', message);
+//     });
 
-    res.render('chat/main', {
-      title: 'Chat'
-     });
-  });
+//     res.render('chat/main', {
+//       title: 'Chat'
+//      });
+//   });
 
-  app.get('/chat/login', function(req, res, io, socket, data){
-    if(!data.nickname){
-      socket.emit('error', {
-        message: 'no nickname provided'
-      });
-      return;
-    }
+//   app.get('/chat/login', function(req, res, io, socket, data){
+//     if(!data.nickname){
+//       socket.emit('error', {
+//         message: 'no nickname provided'
+//       });
+//       return;
+//     }
 
-    Chat.findOne({nickname:data.nickname
-    }, function(err, doc){
-      if(err){
-        socket.emit('error', {
-          message: 'error reading clients list'
-        });
-        return;
-      }
+//     Chat.findOne({nickname:data.nickname
+//     }, function(err, doc){
+//       if(err){
+//         socket.emit('error', {
+//           message: 'error reading clients list'
+//         });
+//         return;
+//       }
 
-      if(doc){
-        console.warn('nickname in use, orphan records?', doc.nickname);
+//       if(doc){
+//         console.warn('nickname in use, orphan records?', doc.nickname);
 
-      socket.emit('login error', {
-        message: 'nickname in use'
-      });
+//       socket.emit('login error', {
+//         message: 'nickname in use'
+//       });
 
-      var client = new Chat();
+//       var client = new Chat();
 
-      client.nickname = data.nickname;
-      client.socket_id = socket.id;
+//       client.nickname = data.nickname;
+//       client.socket_id = socket.id;
 
-      client.save(function(){
-        socket.emit('login ok', {
-          nickname: data.nickname
-        });
+//       client.save(function(){
+//         socket.emit('login ok', {
+//           nickname: data.nickname
+//         });
 
-        exports.clients(io, socket);
-     });
-  	res.render('chat/main', {
-  		title: 'Chat'
-  	})
+//         exports.clients(io, socket);
+//      });
+//   	res.render('chat/main', {
+//   		title: 'Chat'
+//   	})
+};
--- app/controllers/search-controller.js
+++ app/controllers/search-controller.js
@@ -6,44 +6,34 @@ module.exports = function(app){
   });
 
   app.get('/search/:searchNext', function(req, res){
-    searchIndx = req.query.searchInput;
-    pgNum = req.params;
-    console.log(pgNum.searchNext);
-    if(pgNum.searchNext < 1){
-      currentFirst = 1;
-      maxNum = 10;
-      minNum = currentFirst - 1;
-    }else{
-      currentFirst = 5*pgNum.searchNext;
-      maxNum = 10*pgNum.searchNext;
-      minNum = currentFirst -1;
-    }
+    var searchIndx = req.query.searchInput;
+    var pg = req.params;
+    var pgNum = pg.searchNext;
+    var nPerPage = 10;
     if(searchIndx){
-      Question.find({$or :                   // $or is similar to logical || also RegEx allows for partial searchs
-        [ {category: {$regex: searchIndx}},  // Searches by categories
-          {answer:{$regex: searchIndx}},     // Searches by answers
-          {difficulty:{$regex: searchIndx}}, // Searches by difficulty
-          {question: {$regex: searchIndx}},  // Searches the question
-          {year: {$type: 18}}                // Searches by year
-        ] }, function(err, questions){
-      console.log('pgNum: ' + pgNum.searchNext);
-      console.log('currentFirst: ' + currentFirst);
-      console.log('maxNum: ' + maxNum);
-      console.log('minNum: ' + minNum);
-      res.render('questions/search', {
-        title: 'Search',
-        questions: questions,
-        counter: 0,
-        currentFirst: currentFirst,
-        maxNum: maxNum,
-        minNum: minNum
-      });
-    });
+      Question.find({$or :                     // $or is similar to logical || also RegEx allows for partial searchs
+          [ {category: {$regex: searchIndx}},  // Searches by categories
+            {answer:{$regex: searchIndx}},     // Searches by answers
+            {difficulty:{$regex: searchIndx}}, // Searches by difficulty
+            {question: {$regex: searchIndx}},  // Searches the question
+            {year: {$type: 18}}                // Searches by year
+          ] }).skip((pgNum-1)*nPerPage)
+              .limit(nPerPage)
+              .run(function(err, questions){
+                res.render('questions/search', {
+                  title: 'Search',
+                  questions: questions,
+                  currentFirst: pgNum*10,
+                  maxNum: pgNum*15,
+                  minNum: (pgNum*10)-1
+            });
+        });
   }else{
       res.render('questions/search', {
         title: 'Search',
         questions: [],
         counter: 0,
+        searchIndx:searchIndx,
         currentFirst: currentFirst,
         maxNum: maxNum,
         minNum: minNum
